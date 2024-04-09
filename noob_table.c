b3e15323de338240d4a451ebbbcd7620da8f7f65
newtoallofthis123 <noobscience123@gmail.com>
2024-04-09 19:36:21 +0530

    Add Comments

--- noob_table.c
+++ noob_table.c
@@ -1,4 +1,3 @@
-#include <c++/13/variant>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +12,7 @@
 static void resize_up(table *t);
 static void resize_down(table *t);
 
+// is the given number a prime
 int is_prime(const int x) {
   if (x < 2) {
     return -1;
@@ -31,6 +31,7 @@ int is_prime(const int x) {
   return 1;
 }
 
+// Simple function for calculating the next prime
 int next_prime(int x) {
   while (is_prime(x) != 1) {
     x++;
@@ -38,6 +39,7 @@ int next_prime(int x) {
   return x;
 }
 
+// Makes a new item
 static item *new_item(const char *k, const char *v) {
   item *i = malloc(sizeof(item));
   i->key = strdup(k);
@@ -45,6 +47,10 @@ static item *new_item(const char *k, const char *v) {
   return i;
 }
 
+/* new_table_with_size
+ *
+ * Initializes a new table with a given size
+ * */
 table *new_table_with_size(int base_size) {
   table *ht = malloc(sizeof(table));
 
@@ -55,14 +61,17 @@ table *new_table_with_size(int base_size) {
   return ht;
 }
 
+// Creates a new Table and returns a pointer to the table
 table *new_table() { return new_table_with_size(BASE_SIZE); }
 
+// Frees an Item
 static void del_item(item *i) {
   free(i->key);
   free(i->value);
   free(i);
 }
 
+// Frees the table
 void del_table(table *ht) {
   for (int i = 0; i < ht->size; i++) {
     item *item = ht->items[i];
@@ -74,6 +83,10 @@ void del_table(table *ht) {
   free(ht);
 }
 
+/* cal_hash
+ *
+ * Algorithm to calculate the hash
+ * */
 static int cal_hash(const char *s, const long a, const int m) {
   long hash = 0;
   const int len_s = strlen(s);
@@ -84,14 +97,26 @@ static int cal_hash(const char *s, const long a, const int m) {
   return (int)hash;
 }
 
+/* get_hash
+ *
+ * Accepts an index along with the key in order to
+ * implement a double hashing algorithm
+ * */
 static int get_hash(const char *s, const int num_buckets, const int attempt) {
   const int hash_a = cal_hash(s, HT_PRIME_1, num_buckets);
   const int hash_b = cal_hash(s, HT_PRIME_2, num_buckets);
   return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 
-static item HT_DELETED_ITEM = {NULL, NULL};
+// Representes a deleted item.
+// We cannot actuall delete an item completely, so this is to get around to that
+// these are usually flushed out on a resize
+static item DELETED_ITEM = {NULL, NULL};
 
+/* set_item
+ *
+ * Sets the item / inserts an item to the table
+ * */
 void set_item(table *t, const char *key, const char *value) {
   const int load = t->count * 100 / t->size;
   if (load > 70) {
@@ -103,7 +128,7 @@ void set_item(table *t, const char *key, const char *value) {
   int index = get_hash(item->key, t->size, 0);
   curr_item = t->items[index];
   int i = 1;
-  while (curr_item != NULL && curr_item != &HT_DELETED_ITEM) {
+  while (curr_item != NULL && curr_item != &DELETED_ITEM) {
     if (strcmp(curr_item->key, key) == 0) {
       del_item(curr_item);
       t->items[index] = item;
@@ -117,14 +142,18 @@ void set_item(table *t, const char *key, const char *value) {
   t->count++;
 }
 
-static void resize_table(table *t, const int base_size) {
-  if (base_size < BASE_SIZE) {
+/* resize_table
+ *
+ * Resizes the table to another size
+ * */
+static void resize_table(table *t, const int size) {
+  if (size < BASE_SIZE) {
     return;
   }
-  table *new_t = new_table_with_size(base_size);
+  table *new_t = new_table_with_size(size);
   for (int i = 0; i < t->size; i++) {
     item *curr_item = t->items[i];
-    if (curr_item != NULL && curr_item != &HT_DELETED_ITEM) {
+    if (curr_item != NULL && curr_item != &DELETED_ITEM) {
       set_item(new_t, curr_item->key, curr_item->value);
     }
   }
@@ -143,23 +172,35 @@ static void resize_table(table *t, const int base_size) {
   del_table(new_t);
 }
 
+/* resize_up
+ *
+ * Increases the size of the table
+ * */
 static void resize_up(table *t) {
   const int new_size = t->base_size * 2;
   resize_table(t, new_size);
 }
 
+/* resize_down
+ *
+ * Reduces the size of the table
+ * */
 static void resize_down(table *t) {
   const int new_size = t->base_size / 2;
   resize_table(t, new_size);
 }
 
+/* get_item
+ *
+ * Returns the value upon being given the key
+ * */
 char *get_item(table *t, char *key) {
   item *curr_item;
   int index = get_hash(key, t->size, 0);
   curr_item = t->items[index];
   int i = 1;
   while (curr_item != NULL) {
-    if (strcmp(curr_item->key, key) == 0 && curr_item != &HT_DELETED_ITEM) {
+    if (strcmp(curr_item->key, key) == 0 && curr_item != &DELETED_ITEM) {
       return curr_item->value;
     }
 
@@ -171,6 +212,11 @@ char *get_item(table *t, char *key) {
   return NULL;
 }
 
+/* delete_item
+ *
+ * Deletes an Item from the table.
+ * Relies essentially on the index, so O(1) time
+ * */
 void delete_item(table *t, char *key) {
   const int load = t->count * 100 / t->size;
   if (load < 10) {
@@ -180,10 +226,10 @@ void delete_item(table *t, char *key) {
   item *curr_item = t->items[index];
   int i = 1;
   while (curr_item != NULL) {
-    if (curr_item != &HT_DELETED_ITEM) {
+    if (curr_item != &DELETED_ITEM) {
       if (strcmp(curr_item->key, key) == 0) {
         del_item(curr_item);
-        t->items[index] = &HT_DELETED_ITEM;
+        t->items[index] = &DELETED_ITEM;
       }
     }
     index = get_hash(key, t->size, i);
@@ -193,12 +239,15 @@ void delete_item(table *t, char *key) {
   t->count--;
 }
 
-// Print all the items in the table
+/* print_items
+ *
+ * Print out all the items present in the table
+ * */
 void print_items(table *t) {
   item *curr_item;
   for (int i = 0; i < t->size; i++) {
     curr_item = t->items[i];
-    if (curr_item != NULL && curr_item != &HT_DELETED_ITEM) {
+    if (curr_item != NULL && curr_item != &DELETED_ITEM) {
       printf("%d -> %s: %s\n", i, curr_item->key, curr_item->value);
     }
   }
